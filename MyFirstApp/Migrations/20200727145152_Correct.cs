a0923766a6710c191d6b77f88a7edc8cdda31551
nirojasm <nicolasrojasmarin@gmail.com>
2020-07-27 14:36:17 -0500

    Initial commit

---
+++ MyFirstApp/Migrations/20200727145152_Correct.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFirstApp.Migrations
+{
+    public partial class Correct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "nombre",
+                table: "Book",
+                newName: "Nombre");
+
+            migrationBuilder.RenameColumn(
+                name: "autor",
+                table: "Book",
+                newName: "Autor");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Book",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nombre",
+                table: "Book",
+                newName: "nombre");
+
+            migrationBuilder.RenameColumn(
+                name: "Autor",
+                table: "Book",
+                newName: "autor");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Book",
+                newName: "id");
+        }
+    }
+}
---
+++ MyFirstApp/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public String Nombre { get; set; }
+        public String Autor { get; set; }
+
+        public String ISBN { get; set; }
+    }
+}
